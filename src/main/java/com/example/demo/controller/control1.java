4f741af73b664da9c968506614c3e58edffd8af3
Jesper Stroebaek <hamdererindebatman@protonmail.com>
2021-02-24 00:41:02 +0100

    1 push

---
+++ src/main/java/com/example/demo/controller/control1.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+import javax.swing.text.html.HTML;
+
+
+
+@Controller
+public class control1 {
+    @GetMapping("/index")
+    public String index(HTML index){
+
+        return "index";
+
+    }
+
+
+
+}
