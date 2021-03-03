e4aec4f9b7ab0af6d2503f0e5f951575fce18fba
Jesper Stroebaek <hamdererindebatman@protonmail.com>
2021-03-03 11:09:02 +0100

    2 push mainpage added12

--- src/main/java/com/example/demo/controller/control1.java
+++ src/main/java/com/example/demo/controller/control1.java
@@ -6,18 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class control1 {
+public class control1
+{
     @GetMapping("/")
-    public String index(){
-        return "index";
-    }
-
-    @GetMapping("/ghostform")
-    public String ghostform(){return "ghostform";}
-
-
+    public String index(){ return "index";}
 
 }
+
 
 
 
